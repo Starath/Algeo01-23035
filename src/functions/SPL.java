c503307842c897ee1ffca29f829c994ad292eb7a
rayhan-farrukh11 <19623119@std.stei.itb.ac.id>
2024-10-13 13:36:12 +0700

    feat: add SPL constructor

--- src/functions/SPL.java
+++ src/functions/SPL.java
@@ -3,6 +3,27 @@ package functions;
 import matrix.*;
 
 public class SPL {
+    boolean oneSolution;
+    boolean noSolution;
+    boolean infSolution;
+    double[] solutions;
+    
+    // Contructor
+    public SPL(int varCount){
+        this.oneSolution = false;
+        this.noSolution = false;
+        this.infSolution = false;
+        this.solutions = new double[varCount];
+    }
+
+    public boolean oneSolution(){return oneSolution;}
+    public boolean noSolution(){return noSolution;}
+    public boolean infSolution(){return infSolution;}
+    public void setOneSolution() {oneSolution = true;}
+    public void setNoSolution() {noSolution = true;}
+    public void setInfSolution() {infSolution = true;}
+    public void setSolutions(int Idx, int val){solutions[Idx] = val;}
+
     public static Matrix GaussJordanElim (Matrix mProblem) {
         for (int i = 0; i < mProblem.rowCount(); i++) {
             if (mProblem.isPivotZero(i)) {
