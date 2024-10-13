366f1998e46c122b6ba2a191b3d734cb7c01ae14
rayhan-farrukh11 <19623119@std.stei.itb.ac.id>
2024-10-13 22:54:48 +0700

    feat: Main.java determinan

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,6 @@
 package main;
 import java.util.*;
+import matrix.*;
 
 public class Main {
     public static Scanner scan;
@@ -52,7 +53,7 @@ public class Main {
                     System.out.println("3. Metode Matriks balikan");
                     System.out.println("4. Kaidah Cramer");
                     System.out.println("5. Kembali\n");
-                    System.out.print("Pilih menu: ");
+                    System.out.print("Pilih metode: ");
                     pilihanMet = scan.nextInt();
 
                     if (pilihanMet == 1) {break;}
@@ -68,10 +69,21 @@ public class Main {
                     System.out.println("1. Metode Reduksi Baris");
                     System.out.println("2. Metode Ekspansi Kofaktor");
                     System.out.println("3. Kembali\n");
-                    System.out.print("Pilih menu: ");
-                    pilihan = scan.nextInt();
+                    System.out.print("Pilih metode: ");
+                    pilihanMet = scan.nextInt();
 
-                    if (pilihan == 3) {break;}
+                    if (pilihanMet == 3) {break;}
+                    else if(pilihanMet == 1){
+                        Matrix M = inputMatrix(); // Ntar ganti sama inputMatrix
+                        border();
+                        System.out.print("Determinan Matrix: ");
+                        System.out.println(MatrixAdv.detByGauss(M));
+                        border();
+                        
+                    }
+
+                    confirmExit();
+                    break;
                 }
 
                 else if (pilihan == 3) {
@@ -81,7 +93,7 @@ public class Main {
                     System.out.println("1. Metode Reduksi Baris");
                     System.out.println("2. Metode Ekspansi Kofaktor");
                     System.out.println("3. Kembali\n");
-                    System.out.print("Pilih menu: ");
+                    System.out.print("Pilih metode: ");
                     pilihan = scan.nextInt();
 
                     if (pilihan == 3) {break;}
@@ -91,6 +103,22 @@ public class Main {
                 else if (pilihan == 6) {}
                 else if (pilihan == 7) {}
                 else if (pilihan == 8) {
+                    clearScreen();
+                    System.out.println("""                                                                
+                      _____            _____            _____         _____         _____    _____      _____      ______   
+                  ___|\\    \\      ____|\\    \\      ____|\\    \\    ___|\\    \\   ___|\\     \\  |\\    \\    /    /| ___|\\     \\  
+                 /    /\\    \\    /     /\\    \\    /     /\\    \\  |    |\\    \\ |    |\\     \\ | \\    \\  /    / ||     \\     \\ 
+                |    |  |____|  /     /  \\    \\  /     /  \\    \\ |    | |    ||    | |     ||  \\____\\/    /  /|     ,_____/|
+                |    |    ____ |     |    |    ||     |    |    ||    | |    ||    | /_ _ /  \\ |    /    /  / |     \\--'\\_|/
+                |    |   |    ||     |    |    ||     |    |    ||    | |    ||    |\\    \\    \\|___/    /  /  |     /___/|  
+                |    |   |_,  ||\\     \\  /    /||\\     \\  /    /||    | |    ||    | |    |       /    /  /   |     \\____|\\ 
+                |\\ ___\\___/  /|| \\_____\\/____/ || \\_____\\/____/ ||____|/____/||____|/____/|      /____/  /    |____ '     /|
+                | |   /____ / | \\ |    ||    | / \\ |    ||    | /|    /    | ||    /     ||     |`    | /     |    /_____/ |
+                 \\|___|    | /   \\|____||____|/   \\|____||____|/ |____|____|/ |____|_____|/     |_____|/      |____|     | /
+                   \\( |____|/       \\(    )/         \\(    )/      \\(    )/     \\(    )/           )/           \\( |_____|/ 
+                     '   )/           '    '           '    '        '    '       '    '            '             '    )/    
+                         '                                                                                             '     
+                            """);
                     System.exit(0);
                 }
 
@@ -105,19 +133,73 @@ public class Main {
     }
 
     // Pilihan Input Matrix
-    public static double[][] inputMatrix(){
-        double[][] M;
+    public static Matrix inputMatrix(){
+        Matrix M;
+        int pilihan;
+        Scanner scan = new Scanner(System.in);
         System.out.println("");
-        border();
-        System.out.println("Menu Input Matriks");
+        while(true) {
+            clearScreen();
+            border();
+            System.out.println("Menu Input Matriks");
+            border();
+            System.out.println("1. Input dari keyboard");
+            System.out.println("2. Input dari file");
+            System.out.print("Pilih metode: ");
+            pilihan = scan.nextInt();
+
+            if (pilihan == 1){
+                M = MatrixIO.keyboardInputMatrix();
+                break;
+            } else if(pilihan == 2){
+                M = MatrixIO.fileInputMatrix();
+                break;
+            } else {
+                System.out.println("Input tidak valid!\n");
+                confirmExit();
+                M = new Matrix(1,1);
+                M.setElmt(0, 0, 0);
+                return M;
+            }
+
+        }
         return M;
         
     }
     // Pilihan Output Matrix
+    public static void outputMatrix(Matrix M){
+        int pilihan;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("");
+        while(true) {
+            clearScreen();
+            border();
+            System.out.println("Menu Input Matriks");
+            border();
+            System.out.println("1. Output ke terminal");
+            System.out.println("2. Output ke file");
+            System.out.println("3. Output ke terminal & simpan ke file");
+            System.out.print("Pilih metode: ");
+            pilihan = scan.nextInt();
+
+            if (pilihan == 1){
+                MatrixIO.terminalOutputMatrix(M);
+                break;
+            } else if(pilihan == 2){
+                MatrixIO.fileOutputMatrix();
+                break;
+            } else {
+                System.out.println("Input tidak valid!\n");
+                confirmExit();
+                break;
+            }
+
+        }
+    }
 
     // Border untuk styling
     public static void border() {
-        System.out.println("\n+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+\n");
+        System.out.println("+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+");
     }
 
     // Membersihkan terminal
